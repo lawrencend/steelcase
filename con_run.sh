9832c9da32e93405412384a24f9322994307ce43
Nathan Lawrence <Nathan@LawrenceAerospace.com>
2018-03-19 18:22:06 -0500

    app changes

--- con_run.sh
+++
@@ -1,6 +0,0 @@
-pyuic4 steelcase_main_window.ui > steelcase_main_window_ui.py
-pyuic4 steelcase_cpw_dialog.ui > steelcase_cpw_dialog_ui.py
-pyuic4 steelcase_cpfc_dialog.ui > steelcase_cpfc_dialog_ui.py
-pyuic4 steelcase_cop_dialog.ui > steelcase_cop_dialog_ui.py
-pyuic4 steelcase_pw_dialog.ui > steelcase_pw_dialog_ui.py
-python3 steelcase.py
