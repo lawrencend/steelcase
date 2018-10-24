f90bbb562e1eb70b8bc1395076fa8a1a7e57f484
Nathan Lawrence <nathan@lawrenceaerospace.com>
2018-10-24 18:46:55 -0500

    updates

--- src/calibration_routine.py
+++ src/calibration_routine.py
@@ -1,21 +1,25 @@
 from src.test_control import TestControl
-
+from src.load_cell import LoadCell
 
 class CalibrationRoutine():
 
-	def __init__(self):
+    def __init__(self):
 
         # Init parent classes
         super().__init__()
 
-    	self._test_control = TestControl()
-    	self.
+        self._test_control = TestControl()
+        self._load_cell = LoadCell()
 
-	def _setup(self):
-		pass
+    def _setup(self):
+        pass
 
-	def _calibrate(self):
-		pass
+    def _calibrate(self):
+        
+        self._cal_data = self._load_cell.get_calibration_data()
 
-	def _write(self):
-		pass
+    def _write(self):
+
+        with open('.steelcase_cal_data', 'w') as file:
+
+            file.write(self._cal_data)
