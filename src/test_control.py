98fa9594c0c43b20e4ccd4997fa9eb7e9cfb2183
Nathan Lawrence <nathan@lawrenceaerospace.com>
2018-10-28 15:16:49 -0500

    updates

--- src/test_control.py
+++ src/test_control.py
@@ -67,7 +67,7 @@ class TestControl(QObject):
 
 #        raw_load_cell_value = self._pyfirmata.read_load_cell() #
         current_datetime = datetime.now()
-        current_time = datetime.now().time()
+        current_time = float(datetime.now().time())
 
         self._load_cell.update(current_time)
 
